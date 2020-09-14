40031806d83f3ff4ec6f7d4ee0cea28c24911beb
smile <562131444@qq.com>
2020-09-14 11:21:04 +0800

    代码优化

--- lib/productListDemo/main.dart
+++ lib/productListDemo/main.dart
@@ -27,12 +27,21 @@ class HomeContent extends StatelessWidget {
       padding: EdgeInsets.all(8.0),
       child: ListView(
         children: <Widget>[
-          ProductItem('Apple1', 'Macbook1',
-              'https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg'),
-          ProductItem('Apple2', 'Macbook2',
-              'https://tva1.sinaimg.cn/large/006y8mN6gy1g72imm9u5zj30u00k0adf.jpg'),
-          ProductItem('Apple2', 'Macbook3',
-              'https://tva1.sinaimg.cn/large/006y8mN6gy1g72imqlouhj30u00k00v0.jpg')
+          ProductItem(
+              title: 'Apple1',
+              desc: 'Macbook1',
+              imageURL:
+                  'https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg'),
+          ProductItem(
+              title: 'Apple2',
+              desc: 'Macbook2',
+              imageURL:
+                  'https://tva1.sinaimg.cn/large/006y8mN6gy1g72imm9u5zj30u00k0adf.jpg'),
+          ProductItem(
+              title: 'Apple2',
+              desc: 'Macbook3',
+              imageURL:
+                  'https://tva1.sinaimg.cn/large/006y8mN6gy1g72imqlouhj30u00k00v0.jpg')
         ],
       ),
     );
@@ -44,7 +53,12 @@ class ProductItem extends StatelessWidget {
   final String desc;
   final String imageURL;
 
-  ProductItem(this.title, this.desc, this.imageURL);
+  ProductItem({
+    Key key,
+    @required this.title,
+    @required this.desc,
+    @required this.imageURL,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
