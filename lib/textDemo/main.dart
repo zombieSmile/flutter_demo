900a32a23664d786e1bad7756a9c3f70dc05a9fe
smile <562131444@qq.com>
2020-09-15 14:19:33 +0800

    TextDemo

--- lib/textDemo/main.dart
+++ lib/textDemo/main.dart
@@ -14,21 +14,90 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Text Demo'),
         ),
-        body: HomeTextBody(),
+        // body: TextDemo(),
+        // body: TextSpanDemo(),
+        body: DefaultTextStyleDemo(),
       ),
     );
   }
 }
 
-class HomeTextBody extends StatelessWidget {
+class TextDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      '《定风波》 苏轼 \n莫听穿林打叶声，何妨吟啸且徐行。\n竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。',
-      textAlign: TextAlign.center, // 所有内容都居中对齐
-//      maxLines: 1, // 最多显示行数
-//      overflow: TextOverflow.ellipsis, // 超出部分显示...
-      textScaleFactor: 1.25,
+    return Center(
+      child: Text(
+        '《定风波》 苏轼 \n莫听穿林打叶声，何妨吟啸且徐行。\n竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。',
+        textAlign: TextAlign.center, // 所有内容都居中对齐
+        // maxLines: 1, // 最多显示行数
+        // overflow: TextOverflow.ellipsis, // 超出部分显示...
+        // textScaleFactor: 1.25,
+        style: TextStyle(
+          color: Colors.yellow,
+          fontSize: 17.0,
+          height: 1.2,
+          fontFamily: "Courier",
+          background: Paint()..color = Colors.red,
+          decoration: TextDecoration.underline,
+          decorationStyle: TextDecorationStyle.dashed,
+        ),
+      ),
+    );
+  }
+}
+
+class TextSpanDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        children: [
+          TextSpan(
+            text: '《定风波》',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+          TextSpan(
+            text: "\n苏轼",
+            style: TextStyle(fontSize: 18, color: Colors.redAccent),
+          ),
+          TextSpan(
+            text: "\n莫听穿林打叶声，何妨吟啸且徐行。\n竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
+          ),
+        ],
+      ),
+      style: TextStyle(fontSize: 16, color: Colors.black),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class DefaultTextStyleDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      // 设置文本默认样式
+      style: TextStyle(color: Colors.blue, fontSize: 18),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text('Hello Text!'),
+            Text('Hello TextSpan!'),
+            Text(
+              'Hello DefaultTextStyle!',
+              style: TextStyle(
+                inherit: false, // 不继承默认样式
+                color: Colors.red,
+                fontSize: 12,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
