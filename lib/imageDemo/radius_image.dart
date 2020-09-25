588ee9dc9c277bc24dfd23426c6a285fd340ebb1
smile <562131444@qq.com>
2020-09-25 15:37:43 +0800

    RadiusImageDemo

--- lib/imageDemo/radius_image.dart
+++ lib/imageDemo/radius_image.dart
@@ -14,6 +14,69 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('RadiusImage Demo'),
         ),
+        body: RadiusImageDemo(),
+      ),
+    );
+  }
+}
+
+class RadiusImageDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 10),
+          CircleAvatarImage(),
+          SizedBox(height: 10),
+          ClipOvalImage(),
+          SizedBox(height: 10),
+          ClipRRectImage(),
+        ],
+      ),
+    );
+  }
+}
+
+class CircleAvatarImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 100,
+      backgroundImage: NetworkImage(
+          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+      child: Container(
+        alignment: Alignment.center,
+        width: 100,
+        height: 100,
+        child: Text('猫头鹰'),
+      ),
+    );
+  }
+}
+
+class ClipOvalImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Image.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+        width: 100,
+        height: 100,
+      ),
+    );
+  }
+}
+
+class ClipRRectImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+        'https://tva1.sinaimg.cn/large/006y8mN6gy1g7aa03bmfpj3069069mx8.jpg',
+        width: 100,
+        height: 100,
       ),
     );
   }
