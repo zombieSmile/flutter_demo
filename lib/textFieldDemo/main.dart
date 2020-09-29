0cc4a803b18f7ba9f8b620f8a9491ce189deaae5
smile <562131444@qq.com>
2020-09-29 15:02:14 +0800

    TextFieldDemo

--- lib/textFieldDemo/main.dart
+++ lib/textFieldDemo/main.dart
@@ -20,18 +20,60 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TextFieldDemo extends StatelessWidget {
+class TextFieldDemo extends StatefulWidget {
+  @override
+  _TextFieldDemoState createState() => _TextFieldDemoState();
+}
+
+class _TextFieldDemoState extends State<TextFieldDemo> {
+  FocusNode myFocusNode;
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    myFocusNode = FocusNode();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      FocusScope.of(context).requestFocus(myFocusNode);
+    });
+
+    // 设置默认值
+    _textEditingController.text = 'Hello Flutter!';
+    // 文本监听
+    _textEditingController.addListener(() {
+      print('textEditingController:${_textEditingController.text}');
+    });
+    // 选择文本
+    _textEditingController.selection = TextSelection(
+      baseOffset: 2,
+      extentOffset: _textEditingController.text.length,
+    );
+  }
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         TextField(
-          autofocus: true,
+          focusNode: myFocusNode,
           decoration: InputDecoration(
             labelText: '用户名',
             hintText: '用户名或邮箱',
-            prefixIcon: Icon(Icons.person)
+            prefixIcon: Icon(Icons.person),
           ),
+          controller: _textEditingController,
+          onSubmitted: (v) {
+            print('onSubmitted: $v');
+          },
+          onEditingComplete: () {
+            print('onEditingComplete');
+          },
         ),
         TextField(
           decoration: InputDecoration(
@@ -40,10 +82,11 @@ class TextFieldDemo extends StatelessWidget {
             prefixIcon: Icon(Icons.lock),
           ),
           obscureText: true,
+          onChanged: (v) {
+            print('onChange: $v');
+          },
         ),
       ],
     );
   }
 }
-
-
