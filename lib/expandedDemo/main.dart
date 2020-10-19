58171e423de02e7e1a014e595f9b891e9d7195f1
smile <562131444@qq.com>
2020-10-19 15:05:05 +0800

    Expanded、WrapDemo

---
+++ lib/expandedDemo/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expanded Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Expanded Demo'),
+        ),
+        body: ExpandedDemo(),
+      ),
+    );
+  }
+}
+
+class ExpandedDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        // Flex的两个子widget按1：2来占据水平空间
+        Flex(
+          direction: Axis.horizontal,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                height: 30.0,
+                color: Colors.red,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                height: 30.0,
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 20.0),
+          child: SizedBox(
+            height: 100.0,
+            child: Flex(
+              direction: Axis.vertical,
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 30.0,
+                    color: Colors.red,
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 30.0,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/wrapDemo/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wrap Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Wrap Demo'),
+        ),
+        body: WrapDemo(),
+      ),
+    );
+  }
+}
+
+class WrapDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 8.0,
+      runSpacing: 4.0,
+      alignment: WrapAlignment.spaceBetween,
+      children: <Widget>[
+        Chip(
+          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('AH')),
+          label: Text('Hamilton'),
+        ),
+        Chip(
+          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('ML')),
+          label: Text('Lafayette'),
+        ),
+        Chip(
+          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('HM')),
+          label: Text('Mulligan'),
+        ),
+        Chip(
+          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('JL')),
+          label: Text('Laurens'),
+        ),
+      ],
+    );
+  }
+}
