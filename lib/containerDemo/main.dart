d235ad53d39e6906a7d520cbadb9b7489f1d0337
smile <562131444@qq.com>
2020-10-23 20:23:20 +0800

    Update main.dart

--- lib/containerDemo/main.dart
+++ lib/containerDemo/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Container Demo',
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
