065e5e82fcce46aa2d71a4cb639a3a6faba96c3f
zombie_smile <31536005+zombieSmile@users.noreply.github.com>
2020-12-13 23:03:03 +0800

    手势监听

--- lib/gestureDemo/gesture.dart
+++ lib/gestureDemo/gesture.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -14,7 +15,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Gesture Demo'),
         ),
-        body: GestureDetectorDemo(),
+        // body: GestureDetectorDemo(),
+        // body: _DragDemo(),
+        // body: _ScaleDemo(),
+        body: _GestureRecognizerDemo(),
       ),
     );
   }
@@ -28,6 +32,7 @@ class GestureDetectorDemo extends StatefulWidget {
 class _GestureDetectorDemoState extends State<GestureDetectorDemo> {
   // 保存事件名
   String _operation = "无手势触发!";
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -54,5 +59,135 @@ class _GestureDetectorDemoState extends State<GestureDetectorDemo> {
     setState(() {
       _operation = text;
     });
+  }
+}
+
+class _DragDemo extends StatefulWidget {
+  @override
+  __DragDemoState createState() => __DragDemoState();
+}
+
+class __DragDemoState extends State<_DragDemo> {
+  // 距离顶部的偏移
+  double _top = 0.0;
+
+  // 距离左边的偏移
+  double _left = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          top: _top,
+          left: _left,
+          child: GestureDetector(
+            child: CircleAvatar(child: Text('Drag')),
+            // 手指按下回调
+            onPanDown: (DragDownDetails detail) {
+              // 手指按下的相对于屏幕的位置
+              print('用户手指按下：${detail.globalPosition}');
+            },
+            // 手指滑动回调
+            onPanUpdate: (DragUpdateDetails detail) {
+              // 手指滑动更新偏移量
+              setState(() {
+                _left += detail.delta.dx;
+                _top += detail.delta.dy;
+              });
+            },
+            // 手指停止滑动回调
+            onPanEnd: (DragEndDetails detail) {
+              // 滑动结束时在x、y轴上的速度
+              print(detail.velocity);
+            },
+            // // 垂直方向拖动事件
+            // onVerticalDragUpdate: (DragUpdateDetails details) {
+            //   setState(() {
+            //     _top += details.delta.dy;
+            //   });
+            // },
+            // 水平方向拖动事件
+            onHorizontalDragUpdate: (DragUpdateDetails details) {
+              setState(() {
+                _left += details.delta.dx;
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ScaleDemo extends StatefulWidget {
+  @override
+  __ScaleDemoState createState() => __ScaleDemoState();
+}
+
+class __ScaleDemoState extends State<_ScaleDemo> {
+  double _width = 200.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        child: Container(
+          color: Colors.red,
+          width: _width,
+          height: 200.0,
+        ),
+        onScaleUpdate: (ScaleUpdateDetails detail) {
+          // 缩放倍数在0.8到10倍之间
+          _width = 200 * detail.scale.clamp(.8, 10.0);
+        },
+      ),
+    );
+  }
+}
+
+class _GestureRecognizerDemo extends StatefulWidget {
+  @override
+  __GestureRecognizerDemoState createState() => __GestureRecognizerDemoState();
+}
+
+class __GestureRecognizerDemoState extends State<_GestureRecognizerDemo> {
+  TapGestureRecognizer _tapGestureRecognizer = TapGestureRecognizer();
+
+  // 变色开关
+  bool _toggle = false;
+
+  @override
+  void dispose() {
+    // GestureRecognizer一定要调用其dispose方法释放资源
+    _tapGestureRecognizer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text.rich(
+        TextSpan(
+          children: [
+            TextSpan(text: '测试'),
+            TextSpan(
+              text: '点我变色',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: _toggle ? Colors.blue : Colors.red,
+              ),
+              recognizer: _tapGestureRecognizer
+                ..onTap = () {
+                  setState(() {
+                    _toggle = !_toggle;
+                  });
+                },
+            ),
+            TextSpan(text: '测试'),
+          ],
+        ),
+      ),
+    );
   }
 }
