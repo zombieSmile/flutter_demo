2af265fd1f640c365b3fec0a2a71d02e89062ba2
smile <562131444@qq.com>
2020-12-17 16:57:26 +0800

    Update animated_switcher.dart

--- lib/动画/animated_switcher.dart
+++ lib/动画/animated_switcher.dart
@@ -37,10 +37,16 @@ class _AnimatedSwitcherDemoState extends State<AnimatedSwitcherDemo> {
           AnimatedSwitcher(
             duration: const Duration(milliseconds: 500),
             transitionBuilder: (Widget child, Animation<double> animation) {
-              // 执行缩放动画
-              return ScaleTransition(
-                scale: animation,
+              //   // 执行缩放动画
+              //   return ScaleTransition(
+              //     scale: animation,
+              //     child: child,
+              //   );
+              var tween = Tween<Offset>(begin: Offset(1, 0), end: Offset(0, 0));
+              return SlideTransitionX(
+                position: animation,
                 child: child,
+                direction: AxisDirection.up,
               );
             },
             child: Text(
@@ -60,6 +66,70 @@ class _AnimatedSwitcherDemoState extends State<AnimatedSwitcherDemo> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class SlideTransitionX extends AnimatedWidget {
+  SlideTransitionX({
+    Key key,
+    @required Animation<double> position,
+    this.transformHitTests = true,
+    this.direction = AxisDirection.down,
+    this.child,
+  })  : assert(position != null),
+        super(key: key, listenable: position) {
+    // 偏移在内部处理
+    switch (direction) {
+      case AxisDirection.up:
+        _tween = Tween(begin: Offset(0, 1), end: Offset(0, 0));
+        break;
+      case AxisDirection.right:
+        _tween = Tween(begin: Offset(-1, 0), end: Offset(0, 0));
+        break;
+      case AxisDirection.down:
+        _tween = Tween(begin: Offset(0, -1), end: Offset(0, 0));
+        break;
+      case AxisDirection.left:
+        _tween = Tween(begin: Offset(1, 0), end: Offset(0, 0));
+        break;
+    }
+  }
+
+  Animation<double> get position => listenable;
+
+  final bool transformHitTests;
+
+  final Widget child;
+
+  //退场（出）方向
+  final AxisDirection direction;
+
+  Tween<Offset> _tween;
+
+  @override
+  Widget build(BuildContext context) {
+    Offset offset = _tween.evaluate(position);
+    if (position.status == AnimationStatus.reverse) {
+      switch (direction) {
+        case AxisDirection.up:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.right:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+        case AxisDirection.down:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.left:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+      }
+    }
+    return FractionalTranslation(
+      translation: offset,
+      transformHitTests: transformHitTests,
+      child: child,
     );
   }
 }
